7590cc19b9330ed2c443a9420a27300731b2e3a3
ziyi <ziyi_wu@qq.com>
2025-09-22 18:33:06 +0800

    修复上次使用页面跳转逻辑，调整默认页面为恋爱卡片，并增加延迟时间

--- App.vue
+++ App.vue
@@ -18,7 +18,7 @@
 					console.log('上次使用的页面:', lastUsedPage)
 
 					// 如果有记录且不是默认首页，则跳转
-					if (lastUsedPage && lastUsedPage !== 'index') {
+					if (lastUsedPage && lastUsedPage !== 'cards') {
 						setTimeout(() => {
 							let targetUrl = ''
 							switch(lastUsedPage) {
@@ -27,12 +27,12 @@
 									break
 								case 'cards':
 								default:
-									targetUrl = '/pages/index/index'
-									break
+									// 保持在默认的恋爱卡片页面，不需要跳转
+									return
 							}
 
-							if (targetUrl && targetUrl !== '/pages/index/index') {
-								uni.redirectTo({
+							if (targetUrl) {
+								uni.reLaunch({
 									url: targetUrl,
 									success: function(res) {
 										console.log('跳转到上次使用页面成功:', targetUrl)
@@ -42,7 +42,7 @@
 									}
 								})
 							}
-						}, 100) // 延迟一点确保应用完全启动
+						}, 200) // 稍微增加延迟确保应用完全启动
 					}
 				} catch (e) {
 					console.log('检查上次使用页面失败:', e)
